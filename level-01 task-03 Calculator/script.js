30595f06c1a59165310f2011092481c3a6546544
ROSHAN KUMAR <110260144+Roshan-2025@users.noreply.github.com>
2023-07-20 22:30:07 +0530

    Add files via upload

---
+++ level-01 task-03 Calculator/script.js
@@ -0,0 +1,15 @@
+// clear all value
+function clearScreen() {
+    document.getElementById("result").value = "";
+   }
+   
+   //  Display values
+   function display(value) {
+    document.getElementById("result").value += value;
+   }
+   // This function evaluates the expression and return result
+   function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+   }
